8de20e50f0b0d749aa12d7c279c1eb084aead4f5
daviev <davidvalleau@gmail.com>
2022-08-18 21:03:46 -0700

    Creating basic UI

--- script.js
+++ script.js
@@ -22,41 +22,55 @@ function playRound(playerChoice, computerChoice) {
              : -1 // loss
 }
 
-function validChoice(choice) {
-  choice = choice.toLowerCase();
-  return choice == "rock" || choice == "paper" || choice == "scissors";
-}
-
 function game() {
   let wins = 0;
   let losses = 0;
+  let ties = 0;
 
-  for (let i = 0; i < 5; ++i) {
-    let playerChoice = prompt("Choose your fighter!");
+  let rockButton = document.querySelector("#rock");
+  let paperButton = document.querySelector("#paper");
+  let scissorsButton = document.querySelector("#scissors");
+  let resultsDiv = document.querySelector("#results");
 
-    while (!validChoice(playerChoice)) {
-      playerChoice = prompt("Choose from one of: rock, paper, scissors");
+  let winsDiv = document.querySelector("#wins");
+  let lossesDiv = document.querySelector("#losses");
+  let tiesDiv = document.querySelector("#ties");
+
+  let f = (choice) => {
+    if (wins >= 5 || losses >= 5) {
+      return;
     }
 
     let computerChoice = getComputerChoice();
-    let result = playRound(playerChoice, computerChoice);
-
+    let result = playRound(choice, computerChoice);
+    let resultMessage = "";
     if (result == -1) {
-      console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+      resultMessage = `You lose! ${computerChoice} beats ${choice}`;
       losses += 1;
     } else if (result == 0) {
-      console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+      resultMessage = `You win! ${choice} beats ${computerChoice}`;
       wins += 1;
     } else {
-      console.log("You tied!");
+      resultMessage = "You tied!";
+      ties += 1;
     }
-  }
 
-  if (wins > losses) {
-    console.log("Congratulations! You won the best-of-5 series!");
-  } else if (wins < losses) {
-    console.log("Better luck next time!");
-  } else {
-    console.log("Draw!");
-  }
+    if (wins >= 5) {
+      resultMessage = "Congratulations! You were the first to 5 wins!";
+    }
+    if (losses >= 5) {
+      resultMessage = "You lose! You get nothing! Good day sir!";
+    }
+
+    resultsDiv.textContent = resultMessage;
+    winsDiv.textContent = wins.toString();
+    lossesDiv.textContent = losses.toString();
+    tiesDiv.textContent = ties.toString();
+  };
+
+  rockButton.addEventListener("click", () => f("rock"));
+  paperButton.addEventListener("click", () => f("paper"));
+  scissorsButton.addEventListener("click", () => f("scissors"));
 }
+
+game()
